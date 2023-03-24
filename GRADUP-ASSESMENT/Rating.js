00eba607084b247dfeceec1837c67eb7aa2321f6
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-03-24 23:25:52 +0530

    Create Rating.js

---
+++ GRADUP-ASSESMENT/Rating.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rating = (props) => {
+  const stars = props.stars;
+  return (
+    <div className="rating">
+      {[...Array(Math.round(stars <= 5 ? stars : 0)).keys()].map(() => (
+        <div className="star" />
+      ))}
+    </div>
+  );
+};
+
+export default Rating;
