864aa342609b6633f5ccb713bdad5413183f468e
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-03-27 23:41:20 +0530

    Update SongCard.js

--- GRADUP-ASSESMENT/SongCard.js
+++ GRADUP-ASSESMENT/SongCard.js
@@ -2,30 +2,27 @@ import React from "react";
 import { ReactPropTypes } from "react";
 import Rating from "./Rating";
 
-const SongCard = ({value}) => {
-  const { thumb, title, artist, rating } = value.data;
+const SongCard = (props) => {
+  const { thumb, title, artist, rating } = props.data;
 
   return (
     <div className="song-card">
-      <div className="st-thumb">
-        <img src={thumb} alt={title} />
+      <img src={thumb} alt={title} />
+      <div className="song-title">
+        {title} by {artist}
       </div>
-      <div className="details">
-        <div className="song-title">{title}</div>
-        <div className="artist">{artist}</div>
-        <Rating props={...rating} />
-      </div>
+      <Rating stars={rating} />
     </div>
   );
 };
 
-SongCard.prototype = {
-  data: ReactPropTypes.shape({
-    thumb: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    artist: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-  }),
-};
+// SongCard.prototype = {
+//   data: ReactPropTypes.shape({
+//     thumb: PropTypes.string.isRequired,
+//     title: PropTypes.string.isRequired,
+//     artist: PropTypes.string.isRequired,
+//     rating: PropTypes.number.isRequired,
+//   }),
+// };
 
 export default SongCard;
