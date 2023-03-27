e17aa7d6e917e9b8239b3830e991a1648050d2d7
Subhash Kandhway <kandhway002subhash@gmail.com>
2023-03-27 08:48:44 +0530

    Create App.jsx
    App.jsx

---
+++ NewsApp/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import FetchNews from "./FetchNews";
+
+const API_KEY = "418025aa8fda4da6b6412bdd3e270ab4";
+
+function App() {
+  return <FetchNews />;
+}
+
+export default App;
